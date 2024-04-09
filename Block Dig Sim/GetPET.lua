11561b92529ab367458415ec58dbe18f6651a893
Pr0jectNight <166496112+Pr0jectNight@users.noreply.github.com>
2024-04-09 17:09:32 +0800

    Update GetPET.lua

--- Block Dig Sim/GetPET.lua
+++ Block Dig Sim/GetPET.lua
@@ -1,7 +1,7 @@
 local pet = {
     GiftType = "Pet",
     Time = 420,
-    PetName = "Shiny Lucky Cat"
+    PetName = "Shiny Lucky Cat" -- Pet Name to Give (Modify by choice)
 }
 
 game.ReplicatedStorage.Game.RemoteEvents.FreeGift:FireServer(pet)
