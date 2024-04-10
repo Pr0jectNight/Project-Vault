a0ef110bac1690c0edddce29f54abb93fb3a1d19
Pr0jectNight <166496112+Pr0jectNight@users.noreply.github.com>
2024-04-10 12:21:36 +0800

    Update RedeemCodes.lua

--- Warrior Simulator/RedeemCodes.lua
+++ Warrior Simulator/RedeemCodes.lua
@@ -1,8 +1,11 @@
 local codeTable = {
-    [1] = "3000like",
-    [2] = "like15k",
-    [3] = "1000like",
-    [4] = "welcome"
+    "3000like",
+    "like15k",
+    "1000like",
+    "welcome"
 }
 
-game:GetService("ReplicatedStorage").Remotes.Common.Reward.CodeReward:FireServer(unpack(codeTable))
+for _, code in ipairs(codeTable) do
+    game:GetService("ReplicatedStorage").Remotes.Common.Reward.CodeReward:FireServer(code)
+    print("All codes have been redeemed.")
+end
